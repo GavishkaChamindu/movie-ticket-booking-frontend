876ded8b1b9297df7f01c91d4226d7cd19d0f883
GavishkaChamindu <gavichamindu@gmail.com>
2023-10-03 22:09:56 +0530

    fix error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,4 @@
 import "./App.css";
-import Banner from "./pages/banner/Banner";
-import Footer from "./component/footer/Footer";
-import HeaderPage from "./HeaderPage/HeaderPage";
-import HomePage from "./pages/HomePage/HomePage";
-import NewHome from "./pr-3/ShowPages/NewHome";
-import News from "./pages/news/News";
-import Question from "./pages/question/Question";
-import Show from "./pr-3/ShowPages/Show";
 
 function App() {
   return (
@@ -14,7 +6,7 @@ function App() {
   
 
   
-  <News/>
+  
     </div>
   );
 }
