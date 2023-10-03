3cfdc4110ba2b466d7d5f2cc1f97b3fdab81109e
GavishkaChamindu <gavichamindu@gmail.com>
2023-10-03 22:08:41 +0530

    create frontend

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Banner from "./pages/banner/Banner";
+import Footer from "./component/footer/Footer";
+import HeaderPage from "./HeaderPage/HeaderPage";
+import HomePage from "./pages/HomePage/HomePage";
+import NewHome from "./pr-3/ShowPages/NewHome";
+import News from "./pages/news/News";
+import Question from "./pages/question/Question";
+import Show from "./pr-3/ShowPages/Show";
+
+function App() {
+  return (
+    <div>
+  
+
+  
+  <News/>
+    </div>
+  );
+}
+
+export default App;
